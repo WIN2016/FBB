d2bcda57c444fccea8cfdccf9ea5a65fb5b36806
Administrator <Administrator@NBAAC1500097.corp.ais900.org>
2016-04-18 20:53:28 +0700

    Merge origin/master
    Conflicts:
    	README.md

--- frontend/controllers/TestController.php
+++ frontend/controllers/TestController.php
@@ -9,7 +9,13 @@ class TestController extends \yii\web\Controller
         return $this->render('index');
     }
     
-    public function actionTest1(){
+    public function actionTest1()
+    {
         echo 'ทดสอบ 1';
-}
+     }//end test1
+
+     public function actionTest2($name=null,$lname=null){
+       $info = "Your name is $name $lname";
+       return $this->render('test2',['info'=>$info]);
+     }
 }
--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,7 +1,27 @@
 <?php
-
+use yii\helpers\Html;
 /* @var $this yii\web\View */
+$this->title = 'My Yii Application';?>
 
-$this->title = 'My Yii Application';
+<?php
+$route1 = Yii::$app->urlManager->createUrl('test/test1');
 ?>
-TEST1
+<a href="<?=$route1?>">ไปที่ test1</a>
+<br>
+
+<?php
+$route2 = Yii::$app->urlManager->createUrl(['test/test2','name'=>'Ausawin','lname'=>'Ieamsaard']);
+?>
+<a href="<?=$route2?>">ไปที่ test2</a>
+<br>
+
+
+<?=
+Html::a('ลิงค์แบบที่ 3', ['Test/test1']);
+?>
+
+
+<?=
+yii\helpers\Url::to('test/test1');
+
+?>
